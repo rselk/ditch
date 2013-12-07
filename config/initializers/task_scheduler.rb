fc00a1c6447bff666e52ef4b2d864e838cee085c
Ryan Selk <ryanselk@gmail.com>
2013-12-07 14:12:35 -0700

    sending specific emails

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -5,12 +5,14 @@ scheduler.every '15' do
   # Check if record time is past current time
   # if so, send email + mark record as sent (DO NOT DELETE AS NEED TO ALERT USER)
   # if not, do nothing
-  AlertMail.send_email_alert("ryanselk@gmail.com").deliver
-  @test = Alert.where("time_alert > ?", 3)
+  
+  @test = Alert.where("time_alert < ?", Time.now)
+
   @test.each do |p|
-    puts p.to_email
+    puts "SENT EMAIL TO: #{p.to_email}"
+
+    AlertMail.send_email_alert(p.to_email).deliver
   end
-  puts 'Hello... Rufus'
 end
 
 scheduler.join
