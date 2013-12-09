9c89b80d563c0923ab4d3a5089436ed40c7d9fce
Ryan Selk <ryanselk@gmail.com>
2013-12-09 12:58:47 -0700

    js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require jquery.ui.datepicker
 //= require jquery.ui.slider
 //= require jquery-ui-timepicker-addon
