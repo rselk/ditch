59c5aae825d1695852bb5ade09e63816aa707551
Ryan Selk <ryanselk@gmail.com>
2013-12-09 19:17:19 -0700

    twillo sending txt mgs

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,5 +1,15 @@
 scheduler = Rufus::Scheduler.new
 
+#this will have to be hidden in future
+account_sid = 'AC77dd03e7b9af533c85a64ed5360c2bc4'
+auth_token = '630ad66ec565db63058109168c7c16c6'
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+@client.account.messages.create(
+  :from => '+15873162981',
+  :to => '+14038355938',
+  :body => 'RAILS FAILS'
+)
 
 scheduler.every '10' do
   # Check if record time is past current time
