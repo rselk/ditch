08fd2313208ba035a926eb66cc3071aa4ed60bb8
Ryan Selk <ryanselk@gmail.com>
2014-01-10 00:48:55 -0700

    manage page cleaned up

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -1,17 +1,12 @@
 class ManageController < ApplicationController
   def index
     if user_signed_in? 
-      @alerts_false = Alert.where(user_id: current_user, msg_sent: 'f')
-      @txtalerts_false = Txtalert.where(user_id: current_user, msg_sent: 'f' )
+      
       @alerts_true = Alert.where(user_id: current_user, msg_sent: 't')
-      @txtalerts_true = Txtalert.where(user_id: current_user, msg_sent: 't')
+      @alerts_false = Alert.where(user_id: current_user, msg_sent: 'f')
     else
       @alerts_false = []
       @alerts_true= []
-      @txtalerts_false = []
-      @txtalerts_true= []
     end
-
-
   end
 end
